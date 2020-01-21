2bef8b338f905b374617e6b4df3267a76ad3e991
aevbox@gmail.com <aevbox@gmail.com>
2020-01-21 20:42:26 +0300

    FirstComponent development completed

--- shop/src/app/first-component/first-component.component.ts
+++ shop/src/app/first-component/first-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from '../shared/enums/category.enum'
 
 @Component({
   selector: 'app-first-component',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./first-component.component.css']
 })
 export class FirstComponentComponent implements OnInit {
+
+  img: string = 'assets/img/Shirts1.jpg';
+  name: string = 'First component';
+  description: string = 'Firstt Descriprion';
+  price: number = 99.90;
+  category: Category = Category.shirts;
+  isAvailable: boolean = true;
 
   constructor() { }
 
