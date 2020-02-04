9c585d147647b7ac60b89d8410a38a8bebc82f80
aevbox@gmail.com <aevbox@gmail.com>
2020-02-04 22:06:54 +0300

    Implemented  HomeTast 3-4

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LocalStorageService } from './services/local-storage.service';
 import { LocalStoreEngine } from './models/local-store-engine-class';
-
-
+import { ConstantsService } from './services/constants.service';
 
 @NgModule({
   declarations: [],
   providers: [
-    { provide: LocalStorageService, useClass: LocalStoreEngine }
+    { provide: LocalStorageService, useClass: LocalStoreEngine },
+    { provide: ConstantsService, useValue : { App: 'TaskManager', Ver: '1.0' } },
   ],
   imports: [
     CommonModule
