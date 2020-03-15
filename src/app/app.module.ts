4f62df9c392be17487afa428ac0ec7a666c1caec
aevbox@gmail.com <aevbox@gmail.com>
2020-03-15 12:16:18 +0300

    Add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { LoginModule } from './login/login.module';
 import { LayoutModule } from './layout/layout.module';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
 import { NavModule } from './header/nav.module';
@@ -15,6 +16,7 @@ import { OrderModule } from './order/order.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     ProductsModule,
     NavModule,
     SharedModule,
