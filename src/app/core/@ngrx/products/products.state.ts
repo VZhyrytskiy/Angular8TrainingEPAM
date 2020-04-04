f027a4ef667f313a5fa2c8bee925cea6fcf0e8f1
aevbox@gmail.com <aevbox@gmail.com>
2020-04-04 13:16:56 +0300

    Added HomeTask 7 1.1

---
+++ src/app/core/@ngrx/products/products.state.ts
@@ -0,0 +1,45 @@
+import { Product, ProductModel } from './../../../products/models/product.model';
+import { Category } from 'src/app/shared/enums/category.enum';
+import { Sex } from 'src/app/shared/enums/sex.enum';
+
+export interface ProductsState {
+  data: ReadonlyArray<Product>;
+}
+
+export const initialTasksState: ProductsState = {
+  data: [
+    new ProductModel(
+      1,
+      true,
+      'assets/img/Shirt1.jpg',
+      'Shirt1',
+      'Shirt1',
+      10.99,
+      Category.shirts,
+      10,
+      Sex.man
+    ),
+    new ProductModel(
+      2,
+      false,
+      'assets/img/4564303-1.jpg',
+      'Shirt 4564303',
+      '4564303',
+      15.99,
+      Category.shirts,
+      10,
+      Sex.man
+    ),
+    new ProductModel(
+      3,
+      true,
+      'assets/img/5047597-1.jpg',
+      'Shirt 5047597',
+      '5047597',
+      17.99,
+      Category.shirts,
+      10,
+      Sex.man
+    )
+  ]
+};
--- src/app/products/models/product.model.ts
+++ src/app/products/models/product.model.ts
@@ -11,3 +11,17 @@ export interface Product {
   count: number;
   sex: Sex;
 }
+
+export class ProductModel implements Product {
+  constructor(
+    public id: number = null,
+    public isAvailable: boolean = false,
+    public img: string = '',
+    public name: string = '',
+    public description: string = '',
+    public price: number = 0,
+    public category: string = '',
+    public count: number = 0,
+    public sex: Sex
+  ) { }
+}
